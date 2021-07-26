a72d0e92f179c3bf0461e20d34c5c92fb528e528
abdulrehman ajmal <abdulrehman.ajmal@outlook.com>
2021-07-27 01:12:10 +0500

    blog

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import post
+# Create your views here.
+
+
+def home(request):
+    context={
+        'posts':post.objects.all(),
+        'pageTitle':'Posts'
+        }
+    return render(request,'blog/home.html',context)
+
+def about(request):
+    return render(request,'blog/about.html',{'pageTitle':'about'})
