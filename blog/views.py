02380c98cdd2342c977ee6a73444b662ccfb2e55
abdulrehman ajmal <abdulrehman.ajmal@outlook.com>
2021-07-30 04:46:33 +0500

    post view added

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import post
 # Create your views here.
 
 
 def home(request):
-    context={
-        'posts':post.objects.all(),
-        'pageTitle':'Posts'
-        }
-    return render(request,'blog/home.html',context)
+    context = {
+        'posts': post.objects.all(),
+        'pageTitle': 'Posts'
+    }
+    return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = post
+
 
 def about(request):
-    return render(request,'blog/about.html',{'pageTitle':'about'})
+    return render(request, 'blog/about.html', {'pageTitle': 'about'})
