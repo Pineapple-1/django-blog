a6ef1afe2ada5c255035eaf3e2cf60613fe9d684
abdulrehman ajmal <abdulrehman.ajmal@outlook.com>
2021-08-01 23:26:40 +0500

    reset password

--- djangoApp/settings.py
+++ djangoApp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,3 +140,14 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 LOGIN_REDIRECT_URL =  "blog-home"
 
 LOGIN_URL =  "login"
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'dark11364@gmail.com'
+EMAIL_HOST_PASSWORD = 'bzymtyobsmdgmkco'
+
+
+
